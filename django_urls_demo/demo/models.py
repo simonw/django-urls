bd9e7087ae5d9eb0026a5794e7f7342997e387f8
Simon Willison <simon@simonwillison.net>
2008-09-10 23:42:29 +0000

    Improved the demo a bit

--- django_urls_demo/demo/models.py
+++ django_urls_demo/demo/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 from django_urls.base import UrlMixin
 
-class PathDefined(models.Model, UrlMixin):
+"""
+>>> article = ArticleWithPathDefined.objects.create(slug = 'my-article')
+>>> article.get_url()
+'http://localhost/articles/my-article/'
+>>> article.get_url_path()
+'/articles/my-article/'
+>>> asset = AssetWithUrlDefined.objects.create(domain='example.com', filename='logo.png')
+>>> asset.get_url()
+'http://example.com/assets/logo.png'
+>>> asset.get_url_path()
+'/assets/logo.png'
+"""
+
+class ArticleWithPathDefined(models.Model, UrlMixin):
     slug = models.SlugField()
     
     def get_url_path(self):
-        return '/blah/%s/' % self.slug
+        return '/articles/%s/' % self.slug
 
-class UrlDefined(models.Model, UrlMixin):
+class AssetWithUrlDefined(models.Model, UrlMixin):
     domain = models.CharField(max_length=30)
+    filename = models.CharField(max_length = 30)
     
     def get_url(self):
-        return 'http://%s/info.html' % self.domain
+        return 'http://%s/assets/%s' % (self.domain, self.filename)
